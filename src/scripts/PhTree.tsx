f654234af12b88561dca903b8c6efff5cc1ba031
LUCHER4321 <lucher4321@gmail.com>
2025-02-16 17:29:35 -0300

    Update PhTree.tsx

--- src/scripts/PhTree.tsx
+++ src/scripts/PhTree.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Species } from "./Species";
 
 interface PhtreeProps {
@@ -12,20 +13,26 @@ export const PhTree = (
     return (
         <div>
             <svg width={width} height={height}>
-                {DrawTree(commonAncestor, 0, width / commonAncestor.absoluteExtinction(), height / (commonAncestor.allDescendants().length - 1))}
+                {DrawTree(commonAncestor, -1, width / commonAncestor.absoluteExtinction(), height / (commonAncestor.allDescendants().length - 1))}
             </svg>
         </div>
     );
 };
 
 const DrawTree = (species: Species, y: number, scaleX: number, scaleY: number) => {
+    const [showDesc, setShowDesc] = useState(true);
+
+    const changeShowDesc = () => {
+        setShowDesc(!showDesc);
+    };
+
     const spIndex = species.firstAncestor().allDescendants().indexOf(species)
     const startX = species.aparision * scaleX;
-    const endX = startX + species.duration * scaleX;
+    const endX = startX + (showDesc ? species.duration : (species.absoluteExtinction() - species.aparision)) * scaleX;
     const endY = spIndex * scaleY;
     return (
         <g key={spIndex}>
-            {species.ancestor && (
+            {y >= 0 && (
                 <line
                     x1={startX}
                     y1={y}
@@ -40,11 +47,14 @@ const DrawTree = (species: Species, y: number, scaleX: number, scaleY: number) =
                 y={endY}
                 x2={endX}
                 stroke="white"
+                changeShowDesc={changeShowDesc}
                 padding={10}
             />
-            {species.descendants.map((desc, _) => {
-                return DrawTree(desc, endY, scaleX, scaleY);
-            })}
+            {species.descendants.map((desc, _) => (
+                <g style={{ display: showDesc ? 'block' : 'none' }}>
+                    {DrawTree(desc, endY, scaleX, scaleY)}
+                </g>
+            ))}
         </g>
     );
 };
@@ -55,14 +65,17 @@ interface HorizontalLineProps {
     x2: number;
     y: number;
     stroke: string;
+    showDesc?: boolean;
+    changeShowDesc?: () => void;
     padding?: number;
 }
 
-const HorizontalLine = ({species, x1, x2, y, stroke, padding = 0}: HorizontalLineProps) => {
-    const all = species.firstAncestor().allDescendants()
+const HorizontalLine = ({species, x1, x2, y, stroke, showDesc = true, changeShowDesc = () => {}, padding = 0}: HorizontalLineProps) => {
+    const all = species.firstAncestor().allDescendants();
     const index = (s: Species) => all.indexOf(s);
     const orientation = species.ancestor ? (index(species) > index(species.ancestor) ? -3 : 1) : 1;
     const lastOne = species.descendants.filter(desc => desc.aparision === species.extinction()).length === 0;
+    const extinction = showDesc ? species.extinction() : species.absoluteExtinction();
     return (
         <g>
             <line
@@ -74,20 +87,20 @@ const HorizontalLine = ({species, x1, x2, y, stroke, padding = 0}: HorizontalLin
             />
             <foreignObject
                 x={x1 + padding}
-                y={y + (padding) * orientation}
+                y={y + (padding + 5) * orientation}
                 width={x2 - x1 - 2 * padding}
-                height={20}
+                height={50}
             >
                 <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
                     <div>
                         {species.aparision}
                     </div>
-                    <div>
+                    <button>
                         {species.name}
-                    </div>
-                    <div>
-                        {lastOne ? species.extinction() : ""}
-                    </div>
+                    </button>
+                    <button onClick={changeShowDesc}>
+                        {lastOne ? extinction : ""}
+                    </button>
                 </div>
             </foreignObject>
         </g>
