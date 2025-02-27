df4a6a6d3e9ffa1d5aea16e28d9f99034c5a44ec
LUCHER4321 <lucher4321@gmail.com>
2025-02-26 21:56:14 -0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Species } from "./scripts/Species";
+export { PhTree } from "./scripts/PhTree";
+export { Menu } from "./scripts/Menu";
+export { codeText, codeTextAlt, getLanguageOptions } from "./scripts/translate";
