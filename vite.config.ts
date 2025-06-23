d2f74e74f9f3b8f057f547766581dc65f9f078fc
LUCHER4321 <lucher4321@gmail.com>
2025-06-23 18:25:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import dts from 'vite-plugin-dts'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    dts()
+    dts(),
+    viteStaticCopy({
+      targets: [{
+        src: "tsconfig.json",
+        dest: "./"
+      }]
+    })
   ],
   build: {
     lib: {
