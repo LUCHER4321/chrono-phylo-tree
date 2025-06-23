13f15341d415924b936f8541a27ce77076eea3f9
LUCHER4321 <lucher4321@gmail.com>
2025-06-23 18:35:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import dts from 'vite-plugin-dts'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    dts(),
-    viteStaticCopy({
-      targets: [{
-        src: "tsconfig.json",
-        dest: "./"
-      }]
-    })
   ],
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'chrono-phylo-tree',
-      fileName: (format) => `chrono-phylo-tree.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', "react/jsx-runtime"],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          "react/jsx-runtime": "react/jsx-runtime"
-        }
-      }
-    }
-  }
 })
